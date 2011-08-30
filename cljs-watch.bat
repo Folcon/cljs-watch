61f43ad1b4b9f5faa76800fb82702c0e2d3bbdb5
jneira <atreyu.bbb@gmail.com>
2011-08-30 23:05:09 +0200

    changes for windows

---
+++ cljs-watch.bat
@@ -0,0 +1,18 @@
+:@echo off
+setLocal EnableDelayedExpansion
+
+if "%CLOJURESCRIPT_HOME%" == "" goto ERROR_HOME
+
+set CLASSPATH=.;src\;%CLOJURESCRIPT_HOME%src\clj;%CLOJURESCRIPT_HOME%src\cljs"
+for /R "%CLOJURESCRIPT_HOME%\lib" %%a in (*.jar) do (
+  set CLASSPATH=!CLASSPATH!;%%a
+)
+for /R "lib" %%a in (*.jar) do (
+  set CLASSPATH=!CLASSPATH!;%%a
+)
+set CLASSPATH=!CLASSPATH!"
+java -server -Xmx1G -Xms1G -Xmn256m -cp "%CLASSPATH%" clojure.main %~dp0\watcher.clj %*
+goto EOF
+:ERROR_HOME
+echo CLOJURESCRIPT_HOME is not set
+:EOF
